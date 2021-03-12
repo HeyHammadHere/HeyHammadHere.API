b70f947dd14b833d7e03ce0c5b764f69f10b2114
HeyHammadHere <HeyHammadHere@gmail.com>
2021-03-13 02:13:22 +0500

    API Initialized

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const express = require("express");
 const app = express();
 const mysql = require("mysql");
 const path = require("path");
-const port = 3000 || process.env.PORT;
 app.use(express.static(__dirname));
 app.get("/", (req, resp) => {
   resp.sendFile(path.join(__dirname, "index.html"));
   resp.end();
 });
-app.listen(port);
+app.listen(process.env.PORT);
