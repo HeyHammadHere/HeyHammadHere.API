312f94e1d8540d586ed5ebd8b94e9dbc14c27420
HeyHammadHere <HeyHammadHere@gmail.com>
2021-03-15 15:33:01 +0500

    Routes Updated

--- index.js
+++ index.js
@@ -7,10 +7,25 @@ app.get("/", (req, resp) => {
   resp.sendFile(path.join(__dirname, "index.html"));
   resp.end();
 });
+app.get("/dashboard", (res, resp) => {
+  resp.send({ name: "Hammad", age: 22 });
+});
+app.get("/admin-panel", (res, resp) => {
+  resp.send({ name: "Hammad", age: 22 });
+});
+app.get("/documentation", (res, resp) => {
+  resp.send({ name: "Hammad", age: 22 });
+});
 app.get("/api", (res, resp) => {
+  resp.send({ name: "Hammad", age: 22 });
+});
+app.get("/about", (res, resp) => {
+  resp.send({ name: "Hammad", age: 22 });
+});
+app.get("/contact", (res, resp) => {
   resp.send({ name: "Hammad", age: 22 });
 });
 app.get("*", (req, resp) => {
   resp.sendFile(path.join(__dirname, "error.html"));
 });
-app.listen(process.env.PORT);
+app.listen(80);
